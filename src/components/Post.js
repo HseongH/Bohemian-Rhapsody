8bda449d253791c6fd70c9d295b688323f98625e
demian0721 <demian0721@naver.com>
2021-07-10 16:26:57 +0900

    Home 화면 img onclick 전환 추가

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,17 @@
 // LIBRARY
 import React from 'react';
 import { Grid, Image } from '../elements';
+import { history } from '../redux/configStore';
 
 const Post = (props) => {
   return (
     <React.Fragment>
-      <Grid width="252px" radius="20px" margin="10px" hoverShadow>
-        <Image src="http://ui.ssgcdn.com/cmpt/banner/202103/2021032509540448772796294379_265.jpg" />
+      <Grid width="252px" radius="20px" margin="10px" hoverShadow
+        _onClick={() => {
+          history.push('/detail');
+        }}
+      >
+            <Image src="http://ui.ssgcdn.com/cmpt/banner/202103/2021032509540448772796294379_265.jpg" />
       </Grid>
     </React.Fragment>
   )
