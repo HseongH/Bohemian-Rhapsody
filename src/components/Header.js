aebdca1b6dbf842479311b64dc94d72fffaf0068
demian0721 <demian0721@naver.com>
2021-07-10 15:46:44 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 // LIBRARY
 import React from 'react';
+
+//Elements
 import { Input, Grid, Button } from '../elements';
+
+//Icons
+import HomeIcon from '@material-ui/icons/Home';
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <Grid is_flex>
-        <Button>HOME</Button>
+        <Button><HomeIcon/></Button>
         <Input placeholder="Search" margin="0 20px 0" style={{ flex: 1 }} />
-        <Button>Logout</Button>
+        <Button>LOGIN</Button>
       </Grid>
     </React.Fragment>
   );
