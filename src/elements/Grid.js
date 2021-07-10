a98510071189b775baf2b4fdb846ed7642101945
unknown <hh4518@gmail.com>
2021-07-10 11:11:57 +0900

    그리드 요소 수정

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -2,36 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Grid = ({ children, _onClick, ...props }) => {
-
-    return (
-        <React.Fragment>
-            <GridBox {...props} onClick={_onClick} >{children}</GridBox>
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <GridBox {...props} onClick={_onClick}>
+        {children}
+      </GridBox>
+    </React.Fragment>
+  );
+};
 
 Grid.defaultProps = {
-    children: null,
-    is_flex: false,
-    width: "100%",
-    padding: false,
-    margin: false,
-    align: false,
-    bg: false,
-    _onClick: () => { },
-    shadow: false
-}
+  children: null,
+  is_flex: false,
+  width: '100%',
+  padding: false,
+  margin: false,
+  align: false,
+  bg: false,
+  _onClick: () => {},
+  shadow: false,
+};
 
 const GridBox = styled.div`
-    ${(props) => (props.is_flex ? `display: flex; align-iems: center; justify-content: ${props.is_flex};` : '')}
-    width: ${(props) => props.width};
-    height: 100%;
-    ${(props) => (props.padding ? `padding: ${props.padding};` : '')}
-    ${(props) => (props.margin ? `margin: ${props.margin};` : '')}
+  ${(props) =>
+    props.is_flex ? `display: flex; align-iems: center; justify-content: ${props.is_flex};` : ''}
+  width: ${(props) => props.width};
+  height: 100%;
+  ${(props) => (props.padding ? `padding: ${props.padding};` : '')}
+  ${(props) => (props.margin ? `margin: ${props.margin};` : '')}
     ${(props) => (props.align ? `text-align: ${props.align};` : '')}
     ${(props) => (props.bg ? `background-color: ${props.bg};` : '')}
     ${(props) => (props.shadow ? `box-shadow: 5px 9px 15px grey; ` : '')}
+    ${(props) => (props.radius ? `border-radius: ${props.radius};` : '')}
     box-sizing: border-box;
 `;
 
-export default Grid
+export default Grid;
--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,9 +1,29 @@
 // LIBRARY
 import React from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+
+// ELEMENTS
+import { Grid, Button, Input } from '../elements/index';
 
 const Write = (props) => {
-  return <div></div>;
+  return (
+    <Grid
+      is_flex="space-between"
+      margin="0 auto"
+      padding="20px 30px"
+      bg="#eee"
+      style={{ borderRadius: '20px' }}
+    >
+      <Grid padding="10px" style={{ borderRadius: '10px' }}>
+        <label htmlFor="input--file">이미지 추가</label>
+        <input type="file" id="input--file" />
+      </Grid>
+
+      <Grid>
+        <Input fontSize="23px" style={{ fontWeight: 700 }} />
+      </Grid>
+    </Grid>
+  );
 };
 
 Write.defaultProps = {};
