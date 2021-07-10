45beb02274d73aa5a0d33164f25aae5ca61787b7
unknown <hh4518@gmail.com>
2021-07-10 18:17:29 +0900

    포스트 리덕스 작업 중간 저장

--- src/redux/modules/detailPost.js
+++ src/redux/modules/detailPost.js
@@ -1,20 +1,60 @@
+// AXIOS
+import instance from '../../common/axios';
+
 // ACTION
-const DETAIL_POST = 'DETAIL_POST';
+const POST_CREATE = 'POST_CREATE';
+const POST_DETAIL = 'POST_DETAIL';
+const POST_UPDATE = 'POST_UPDATE';
+const POST_DELETE = 'POST_DELETE';
 
 // ACTION CREATER
-export const readPost = (post) => ({ type: DETAIL_POST, post });
+const createPost = (post) => ({ type: POST_CREATE, post });
+const getOnePost = (post) => ({ type: POST_DETAIL, post });
+const updatePost = (postId, post) => ({ type: POST_UPDATE, postId, post });
 
 // INITIAL STATE
-const initialState = {};
+const initialState = {
+  post: null,
+};
 
 // MIDDLEWARE
-const getOnePost = () => {};
+const getOntPostDB = (postId) => {
+  return function (dispatch) {
+    instance
+      .get(`/detail/:${postId}`)
+      .then((res) => {
+        console.log(res);
+        dispatch(getOnePost(res));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+};
+
+const updatePostDB = (postId, post) => {
+  return function (dispatch) {
+    instance
+      .put(`/api/post/:${postId}`, { ...post })
+      .then((res) => {
+        console.log(res);
+        dispatch(getOnePost(res));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+};
 
 // REDUCER
 function postDetail(state = initialState, action) {
   switch (action.type) {
-    case DETAIL_POST:
-      return state;
+    case POST_DETAIL:
+      if (action.post)
+        return {
+          post: action.post,
+        };
+      return null;
 
     default:
       return state;
@@ -22,3 +62,10 @@ function postDetail(state = initialState, action) {
 }
 
 export default postDetail;
+
+export const postActions = {
+  createPost,
+  getOnePost,
+  updatePost,
+  getOnePost,
+};
