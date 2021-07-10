d5a18cdfe6218a49fd3414520525b41a3a0aefb0
demian0721 <demian0721@naver.com>
2021-07-10 09:16:17 +0900

    elementsIndex
    묶어서 익스포트 했습니다.

---
+++ src/elements/index.js
@@ -0,0 +1,9 @@
+import Button from "./Button";
+import Favorite from "./Favorite";
+import Grid from "./Grid";
+import Image from "./Image";
+import Input from "./Input";
+import Text from "./Text";
+import Title from "./Title";
+
+export { Button, Favorite, Grid, Image, Input, Text, Title};
