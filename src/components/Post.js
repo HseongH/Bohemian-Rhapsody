9804f976c710baf8508978758574b5714dfc6381
demian0721 <demian0721@naver.com>
2021-07-10 16:22:36 +0900

    그림자효과 수정

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,7 +5,7 @@ import { Grid, Image } from '../elements';
 const Post = (props) => {
   return (
     <React.Fragment>
-      <Grid width="252px" radius="20px" margin="10px">
+      <Grid width="252px" radius="20px" margin="10px" hoverShadow>
         <Image src="http://ui.ssgcdn.com/cmpt/banner/202103/2021032509540448772796294379_265.jpg" />
       </Grid>
     </React.Fragment>
