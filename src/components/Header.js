8eb3e54729cff82e631b461625436b41591768ef
demian0721 <demian0721@naver.com>
2021-07-10 16:34:51 +0900

    홈버튼 클릭이벤트 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,13 @@ import { Input, Grid, Button } from '../elements';
 
 //Icons
 import HomeIcon from '@material-ui/icons/Home';
+import { history } from '../redux/configStore';
 
 const Header = (props) => {
   return (
     <React.Fragment>
       <Grid is_flex>
-        <Button><HomeIcon/></Button>
+        <Button clickEvent={() => {history.push('/');}}><HomeIcon/></Button>
         <Input placeholder="Search" margin="0 20px 0" style={{ flex: 1 }} />
         <Button>LOGIN</Button>
       </Grid>
