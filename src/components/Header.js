90d5b07a9be6b1fc1fb422ab5909edd593aa1271
demian0721 <demian0721@naver.com>
2021-07-12 13:34:45 +0900

    로그인 구현 중... 중간 저장
    현재 로그인 유지 기능 구현 안되있음.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,65 @@
-// LIBRARY
+//LIBRARY
 import React from 'react';
 
-//Elements
+//ELEMENTS
 import { Input, Grid, Button } from '../elements';
 
-// COMPONENTS
+//COOKIE
+import { getCookie, deleteCookie } from '../prac/Cookie';
+
+//COMPONENTS
 import Dropdown from './Dropdown';
 
-// HISTORY
+//HISTORY
 import { history } from '../redux/configStore';
+
+//REDUX-ACTION & REACT-HOOK
+import { actionCreators as userActions } from '../redux/modules/user';
+import { useSelector, useDispatch } from 'react-redux';
 
 //Icons
 import HomeIcon from '@material-ui/icons/Home';
 import PersonIcon from '@material-ui/icons/Person';
-import SearchIcon from '@material-ui/icons/Search';
 
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const is_login = useSelector((state) => state.user.is_login);
+
+  if (is_login) {
+    return (
+      <React.Fragment>
+        <Grid is_flex height="100px">
+          <Button
+            clickEvent={() => {
+              history.push('/');
+            }}
+          >
+            <HomeIcon />
+          </Button>
+
+          <Input padding="20px 15px" placeholder="Search" margin="0 20px 0" style={{ flex: 1 }} />
+
+          <Dropdown
+            contents={['로그아웃', '즐겨찾기']}
+            clickEvent={[
+              () => {
+                dispatch(userActions.logOut({}));
+                history.push('/login');
+              },
+              () => {
+                history.push('/detail');
+              },
+            ]}
+            icon={<PersonIcon />}
+            top="83px"
+            pos="110px"
+          />
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
   return (
     <React.Fragment>
       <Grid is_flex height="100px">
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,25 +1,60 @@
-// ACTION
-const USER = 'USER';
+// REDUX-ACTIONS & IMMER
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
 
-// ACTION CREATER
-export const readUser = (user) => ({ type: USER, user });
+// COOKIE
+import { setCookie, getCookie, deleteCookie } from "../../prac/Cookie";
+
+// ACTION
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+const GET_USER = "GET_USER";
+
+// ACTION CREATORS
+const logIn = createAction(LOG_IN, (user) => ({ user }));
+const logOut = createAction(LOG_OUT, (user) => ({ user }));
+const getUser = createAction(GET_USER, (user) => ({ user }));
 
 // INITIAL STATE
 const initialState = {
-  user: [],
+  user: null,
+  is_login: false,
 };
 
 // MIDDLEWARE
-
-// REDUCER
-function user(state = initialState, action) {
-  switch (action.type) {
-    case USER:
-      return state;
-
-    default:
-      return state;
+const loginAction = (user) => {
+  return function (dispatch, getState, { history }) {
+    console.log(history);
+    dispatch(logIn(user));
+    history.push('/');
   }
 }
 
-export default user;
+// REDUCER
+export default handleActions(
+  {
+    [LOG_IN]: (state, action) =>
+      produce(state, (draft) => {
+        setCookie("is_login", "success");
+        draft.user = action.payload.user;
+        draft.is_login = true;
+      }),
+    [LOG_OUT]: (state, action) =>
+      produce(state, (draft) => {
+        deleteCookie("is_login");
+        draft.user = null;
+        draft.is_login = false;
+      }),
+    [GET_USER]: (state, action) => produce(state, (draft) => { }),
+  },
+  initialState
+);
+
+const actionCreators = {
+  logIn,
+  getUser,
+  logOut,
+  loginAction,
+};
+
+export { actionCreators };
